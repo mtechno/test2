5194a06cf765d6fa9f5745c930b9d135a8c0aead
Javatech Roman <mtechnolog@yandex.ru>
2017-02-03 00:10:50 +0300

    first add

---
+++ level03/lesson12/home02/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level03.lesson12.home02;
+
+/* Реализовать метод setName
+Реализовать метод setName, чтобы с его помощью можно было
+ устанавливать значение переменной private String name равное переданному параметру String name.
+*/
+
+public class Solution
+{
+
+    private static int catsCount = 0;
+
+    public static void setCatsCount(int catsCount) {
+        //add your code here
+
+    }
+    }
+
